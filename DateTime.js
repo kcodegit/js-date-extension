7708455ae13eb176678ca808e5d739b73cb1bade
Kohei Ando <andokohei@gmail.com>
2018-01-08 01:46:08 +0000

    LANG modified

--- DateTime.js
+++ DateTime.js
@@ -11,9 +11,26 @@
  * Date class extension
  */
 class DateTime extends Date {
-  constructor(){
-    super();
+  constructor(...str){
+    if(!(arguments.length)) super();
+    else super(str);
+    this.lang = LANG.en;
   }
+
+  /*
+    custom functions
+  */
+
+  /**
+   * set lang
+   * @param { int } lang (enum LANG)
+   * @throws { Error }
+   */
+  setLang(lang){
+    if(!_isLANG(lang)) throw new Error('Invalid Arguments. Must be LANG.');
+    this.lang = lang;
+  }
+
   /**
    * returns time in a format given in the argument
    * @param { string } format_str
@@ -60,9 +77,36 @@ class DateTime extends Date {
     if(arguments.length !== 1) throw new Error('Invalid Arguments.');
     return !(date instanceof Date) ? false : this.getTime() === date.getTime();
   }
+
+  /*
+    override
+  */
+  /**
+   * get the day string of target language given in the argument
+   * if no target is specified, returns the default lang of the instance
+   * @param { object } target_lang (enum LANG) optional
+   * @returns { string }
+   * @throws { Error }
+   */
+  getDay(target_lang = this.lang) {
+    if(!_isLANG(target_lang)) throw new Error('Invalid Arguments. Must be LANG.');
+    return target_lang.weekdays[super.getDay()];
+  }
 }
 
+// non-exported functions
+var _isLANG = l => Object.keys(LANG).some(k => LANG[k] === l);
 
+// enum
+const LANG = Object.freeze({
+  en : {
+    toString: _ => 'English',
+    weekdays: ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday']
+  },
+  jp : {
+    toString: _ => '日本語',
+    weekdays: ['日曜日', '月曜日', '火曜日', '水曜日', '木曜日', '金曜日', '土曜日']
+  }
+})
 
-
-module.exports = DateTime;
+module.exports = { DateTime: DateTime, LANG: LANG };
--- test/datetime_test.js
+++ test/datetime_test.js
@@ -1,12 +1,17 @@
 'use strict';
 
 // imports
-var DateTime = require('../DateTime');
+var DateTime = require('../DateTime').DateTime;
+var LANG = require('../DateTime').LANG;
 // variables
 var p = console.log;
 
 var d1 = new DateTime();
-var d2 = new DateTime();
+var d2 = new DateTime(1988,5,14);
 p(d1.getTime());
-p(d2.getTime());
+p(d2.getFullYear());
+d2.setLang(LANG.jp);
+p(d2.getDay(LANG.en))
+p(d2.getDay())
+
 p(d1.equals(d2));
